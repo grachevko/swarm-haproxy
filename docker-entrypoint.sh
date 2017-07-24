0f6459293675d21ee3227ed901db06ade5444b35
Konstantin Grachev <me@grachevko.ru>
2017-07-24 15:01:48 +0300

    Handle hosts async

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,87 @@ BACKEND_RESOLVER=" resolvers dns check inter 1000"
 BACKEND_HEADER_FORWARDED="	http-request add-header X-Forwarded-Proto https if { ssl_fc }
 	http-request set-header X-Forwarded-Port %[dst_port] if { ssl_fc }"
 
+handle_host () {
+	ENV_PREFIX=$1
+	FILE_PREFIX=$2
+	REDIRECT_PREFIX=$3
+	index=$4
+
+    IFS='=' read id options <<< "$i"
+    id=${id:${#ENV_PREFIX}}
+
+    if [[ ${id:0:8} == "$REDIRECT_PREFIX" ]] ; then
+        IFS=';' read hosts location code scheme <<< "$options"
+    else
+        IFS=';' read hosts service <<< "$options"
+        IFS=':' read host port <<< "$service"
+
+        if ! nslookup "$host" 127.0.0.11 &> /dev/null
+        then
+            >&2 echo service host \""$service"\" is unreachable and will be skipped
+
+            continue
+        fi
+    fi
+
+	### Frontend
+    FRONT_FILE="$HAPROXY_CFG_DIR"/conf.d/$((index + FILE_PREFIX))-"$id"
+
+    IFS=',' read -ra HOSTS <<< "$hosts"
+    host_index=0
+    condition=""
+    for host in "${HOSTS[@]}"; do
+        ((host_index++))
+
+        negate=""
+        if [[ ${host:0:1} == "!" ]] ; then
+            host=${host:1}
+            negate="!"
+        fi
+        condition="${condition} ${negate}${id}_${host_index}"
+
+        echo "" >> "$FRONT_FILE"
+        if [[ ${host:0:1} == "/" ]] ; then
+            echo -n "$ACL_PATH" >> "$FRONT_FILE"
+        else
+            echo -n "$ACL_HOST" >> "$FRONT_FILE"
+        fi
+
+        sed -i "s~%id%~"$id"_"$host_index"~g" "$FRONT_FILE"
+        sed -i "s~%host%~$host~g" "$FRONT_FILE"
+    done
+    echo -e "" >> "$FRONT_FILE"
+
+    if [[ ${id:0:8} == "$REDIRECT_PREFIX" ]] ; then
+        echo "$REDIRECT" >> "$FRONT_FILE"
+
+        sed -i "s~%id%~"$id"_1~g" "$FRONT_FILE" // _1 костыль
+        sed -i "s~%location%~$location~g" "$FRONT_FILE"
+        sed -i "s~%code%~$code~g" "$FRONT_FILE"
+        sed -i "s~%scheme%~$scheme~g" "$FRONT_FILE"
+
+        continue
+    else
+        echo "$USE_BACKEND" >> "$FRONT_FILE"
+        sed -i "s~%id%~$ENV_PREFIX$id~g" "$FRONT_FILE"
+        sed -i "s~%condition%~$condition~g" "$FRONT_FILE"
+    fi
+
+	### Backend
+    BACK_FILE="$HAPROXY_CFG_DIR"/conf.d/$((index + 500))-"$id"
+
+    echo -n "$BACKEND_START" > "$BACK_FILE"
+    if [[ "$service" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+        echo "" >> "$BACK_FILE"
+    else
+        echo "$BACKEND_RESOLVER" >> "$BACK_FILE"
+    fi
+    echo "$BACKEND_HEADER_FORWARDED" >> "$BACK_FILE"
+
+    sed -i "s~%id%~$ENV_PREFIX$id~g" "$BACK_FILE"
+    sed -i "s~%service%~$service~g" "$BACK_FILE"
+}
+
 index=0
 handle_http () {
 	ENV_PREFIX=$1
@@ -27,80 +108,10 @@ handle_http () {
 	for i in `env | fgrep "$ENV_PREFIX"`; do
 		((index++))
 
-		IFS='=' read id options <<< "$i"
-		id=${id:${#ENV_PREFIX}}
-
-		if [[ ${id:0:8} == "$REDIRECT_PREFIX" ]] ; then
-			IFS=';' read hosts location code scheme <<< "$options"
-		else
-			IFS=';' read hosts service <<< "$options"
-			IFS=':' read host port <<< "$service"
-
-			if ! nslookup "$host" 127.0.0.11 &> /dev/null
-			then
-			    >&2 echo service host \""$service"\" is unreachable and will be skipped
-
-			    continue
-			fi
-		fi
-
-	### Frontend
-		FRONT_FILE="$HAPROXY_CFG_DIR"/conf.d/$((index + FILE_PREFIX))-"$id"
-
-		IFS=',' read -ra HOSTS <<< "$hosts"
-		host_index=0
-		condition=""
-		for host in "${HOSTS[@]}"; do
-            ((host_index++))
-
-            negate=""
-            if [[ ${host:0:1} == "!" ]] ; then
-                host=${host:1}
-                negate="!"
-            fi
-            condition="${condition} ${negate}${id}_${host_index}"
-
-            echo "" >> "$FRONT_FILE"
-			if [[ ${host:0:1} == "/" ]] ; then
-				echo -n "$ACL_PATH" >> "$FRONT_FILE"
-			else
-				echo -n "$ACL_HOST" >> "$FRONT_FILE"
-			fi
-
-			sed -i "s~%id%~"$id"_"$host_index"~g" "$FRONT_FILE"
-			sed -i "s~%host%~$host~g" "$FRONT_FILE"
-		done
-		echo -e "" >> "$FRONT_FILE"
-
-		if [[ ${id:0:8} == "$REDIRECT_PREFIX" ]] ; then
-			echo "$REDIRECT" >> "$FRONT_FILE"
-
-			sed -i "s~%id%~"$id"_1~g" "$FRONT_FILE" // _1 костыль
-			sed -i "s~%location%~$location~g" "$FRONT_FILE"
-			sed -i "s~%code%~$code~g" "$FRONT_FILE"
-			sed -i "s~%scheme%~$scheme~g" "$FRONT_FILE"
-
-			continue
-		else
-			echo "$USE_BACKEND" >> "$FRONT_FILE"
-			sed -i "s~%id%~$ENV_PREFIX$id~g" "$FRONT_FILE"
-			sed -i "s~%condition%~$condition~g" "$FRONT_FILE"
-		fi
-
-	### Backend
-		BACK_FILE="$HAPROXY_CFG_DIR"/conf.d/$((index + 500))-"$id"
-
-		echo -n "$BACKEND_START" > "$BACK_FILE"
-		if [[ "$service" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-			echo "" >> "$BACK_FILE"
-		else
-			echo "$BACKEND_RESOLVER" >> "$BACK_FILE"
-		fi
-		echo "$BACKEND_HEADER_FORWARDED" >> "$BACK_FILE"
-
-		sed -i "s~%id%~$ENV_PREFIX$id~g" "$BACK_FILE"
-		sed -i "s~%service%~$service~g" "$BACK_FILE"
+        handle_host ${ENV_PREFIX} ${FILE_PREFIX} ${REDIRECT_PREFIX} ${index} &
 	done
+
+	wait
 }
 
 if env | fgrep HTTP_ > /dev/null; then
